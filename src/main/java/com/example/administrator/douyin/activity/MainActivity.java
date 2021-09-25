fd838136fa39733faa1fee1eee74f05b878603cc
xcodeju <juruilin@1314>
2021-09-25 20:04:51 +0800

    4.点击我的跳转到个人界面

--- src/main/java/com/example/administrator/douyin/activity/MainActivity.java
+++ src/main/java/com/example/administrator/douyin/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.douyin.activity;
 
+
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.douyin.util.MyAdapter;
 import com.example.administrator.douyin.manager.MyLayoutManager2;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private MyAdapter mAdapter;
     MyLayoutManager2 myLayoutManager;
+    //我的界面按钮
+    private TextView mHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initListener();
+        initEvent();
         hideBottomUIMenu();
     }
 
     /**
      * 初始化控件
      */
+
     private void initView() {
+
         mRecyclerView = findViewById(R.id.recycler);
         myLayoutManager = new MyLayoutManager2(this, OrientationHelper.VERTICAL, false);
 
@@ -44,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(myLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
+        mHome = findViewById(R.id.me);
+
     }
 
     /**
-     * 设置监听
+     * 实现滑动监听
      */
     private void initListener() {
         myLayoutManager.setOnViewPagerListener(new OnViewPagerListener() {
@@ -73,6 +84,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "选择位置:" + position + " 下一页:" + bottom);
 
                 PlayVideoManager.getInstance(mRecyclerView).playVideo(0);
+            }
+        });
+    }
+
+    /**
+     * 设置点击事件
+     * */
+    private void initEvent(){
+        mHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PersonalActivity.class);
+                startActivity(intent);
             }
         });
     }
