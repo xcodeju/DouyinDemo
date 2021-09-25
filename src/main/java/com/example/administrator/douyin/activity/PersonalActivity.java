cb845606f6c46392ec4b1792419611878797ab75
xcodeju <juruilin@1314>
2021-09-25 19:34:48 +0800

    3.完善用户个人界面

--- src/main/java/com/example/administrator/douyin/activity/PersonalActivity.java
+++ src/main/java/com/example/administrator/douyin/activity/PersonalActivity.java
@@ -2,8 +2,12 @@ package com.example.administrator.douyin.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.administrator.douyin.R;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class PersonalActivity extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class PersonalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+        blurBackGround();
+    }
+
+    /**
+     * 模糊背景实现
+     * */
+    private void blurBackGround(){
+        ImageView mBgImage = findViewById(R.id.iv_bg);
+        //背景模糊实现
+        Glide.with(PersonalActivity.this).load(R.mipmap.bg_blure).bitmapTransform(new BlurTransformation(PersonalActivity.this,20,2))
+                .into(mBgImage);
     }
 }
